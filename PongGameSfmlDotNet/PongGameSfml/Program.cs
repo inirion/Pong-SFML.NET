e6c9cfb96b28c959978ff59df222b5e29499f949
Grzegorz Kokoszka <grzegorz.r.kokoszka@gmail.com>
2017-08-23 19:19:09 +0200

    Working startup Project

---
+++ PongGameSfmlDotNet/PongGameSfml/Program.cs
@@ -0,0 +1,26 @@
+﻿using SFML.Graphics;
+using SFML.Window;
+
+namespace PongGameSfml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RenderWindow window = new RenderWindow(new VideoMode(1000, 800), "SFML works!", Styles.Default);
+
+            window.Closed += (sender, arg) => window.Close();
+            CircleShape shape = new CircleShape(100.0f);
+            shape.FillColor = Color.Green;
+
+            while (window.IsOpen)
+            {
+                window.DispatchEvents();
+
+                window.Clear(Color.Black);
+                window.Draw(shape);
+                window.Display();
+            }
+        }
+    }
+}
---
+++ PongGameSfmlDotNet/PongGameSfmlDotNet/Program.cs
@@ -0,0 +1,13 @@
+﻿using SFML.Graphics;
+using SFML.Window;
+
+namespace PongGameSfmlDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RenderWindow window = new RenderWindow(new VideoMode(800,600),"Pong",Styles.Default);
+        }
+    }
+}
